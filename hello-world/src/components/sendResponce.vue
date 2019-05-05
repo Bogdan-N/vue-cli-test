db4de819c69cb5bf2b9212cf09f0dd88ce19fd71
Bogdan-N <nosachenko.bogdan@gmail.com>
2019-05-05 20:52:59 +0300

    first commit

---
+++ hello-world/src/components/sendResponce.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="button">
+    <button @click="send">{{someText}}</button>
+    <pre>{{ result.name }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      someText: "Send a request",
+      result: ''
+    };
+  },
+  methods: {
+    send() {
+      this.axios.get('http://localhost:3003/user')
+      .then(response => {
+        this.result = response.data;
+        console.log('this.result ', this.result);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  }
+};
+</script>
+
+<style>
+.button {
+  margin: 20px auto;
+}
+</style>
