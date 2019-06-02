29168625a01306cc07e0639a966cdab41542f51a
Bogdan-N <nosachenko.bogdan@gmail.com>
2019-06-02 23:03:20 +0300

    dynamic rendering of section height

--- my-creation/src/components/welcomPage.vue
+++ my-creation/src/components/welcomPage.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="welcomPage-1">
-    <section class="block-A">
+    <section class="block first">
       <div>
         <button>ON section A</button>
       </div>
     </section>
-    <section class="block-B" :class="{ 'active': someVAriable }">
+    <section class="block second">
       <div>
         <button>ON section B</button>
       </div>
     </section>
-    <section class="block-C">
+    <section class="block third">
       <div>
         <button>ON section C</button>
       </div>
@@ -20,27 +20,39 @@
 
 <script>
 export default {
-  name: 'welcomPage',
+  name: "welcomPage",
   props: {
     msg: String
   },
-  data () {
+  data() {
     return {
+      currentHeight: 0,
       someVAriable: false
-    }
+    };
   },
   methods: {
-    onScroll (event) {
-      this.someVAriable = true
-      event.preventDefault()
-      // setTimeout(() => this.someVAriable = false, 3000);
+    onScroll(event) {
+      this.someVAriable = true;
+      event.preventDefault();
+    },
+    setSectionsHeight(dynamicHeight) {
+      const elements = document.getElementsByClassName("block");
+      console.log("elements ", elements);
+      for (let i = 0; i < elements.length; ++i) {
+        elements[i].style.height = `${dynamicHeight}px`;
+      }
     }
   },
-  created: function () {
-    window.addEventListener('scroll', this.onScroll)
+  created: function() {
+    window.addEventListener("scroll", this.onScroll);
   },
-  destroyed: function () {
-    window.removeEventListener('scroll', this.onScroll)
+  mounted: function() {
+    const currentHeight = window.innerHeight;
+    console.log("currentHeight ", currentHeight);
+    this.setSectionsHeight(currentHeight);
+  },
+  destroyed: function() {
+    window.removeEventListener("scroll", this.onScroll);
   }
-}
+};
 </script>
